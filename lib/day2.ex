00204ecf6d4ce412f7d09fa7d2aae96da6cac57e
Stefan Kupych <stefan@hifyre.com>
2021-12-02 01:16:41 -0500

    Day 2 Solution

--- lib/day2.ex
+++ lib/day2.ex
@@ -10,19 +10,51 @@ defmodule Aoc.Day2 do
   def day(), do: 2
 
   @impl Day
-  def a(_) do
-    :ok
+  def a(instructions) do
+    instructions
+    |> Enum.reduce([0, 0], &parse_instruction/2)
+    |> Enum.product()
   end
 
   @impl Day
-  def b(_) do
-    :ok
+  def b(instructions) do
+    instructions
+    |> Enum.reduce([0, 0, 0], &parse_instruction/2)
+    |> tl() 
+    |> Enum.product()
   end
 
   @impl Day
   def parse_input() do
     with {:ok, file} <- Day.load(__MODULE__) do
       file
+      |> String.split(~r/\s/, trim: true)
+      |> Enum.chunk(2)
+      |> Enum.map(fn [command, value] -> {command, String.to_integer(value)} end)
     end
+  end
+
+  defp parse_instruction({"forward", value}, [x, z]) do
+    [x + value, z]
+  end
+
+  defp parse_instruction({"up", value}, [x, z]) do
+    [x, z - value]
+  end
+
+  defp parse_instruction({"down", value}, [x, z]) do
+    [x, z + value]
+  end
+
+  defp parse_instruction({"forward", value}, [aim, x, z]) do
+    [aim, x + value, z + value * aim]
+  end
+
+  defp parse_instruction({"up", value}, [aim, x, z]) do
+    [aim - value, x, z]
+  end
+
+  defp parse_instruction({"down", value}, [aim, x, z]) do
+    [aim + value, x, z]
   end
 end
