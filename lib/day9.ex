7443ae408dc573e018d8d1b4609776ef04422a23
Stefan Kupych <stefan@hifyre.com>
2021-12-11 14:25:46 -0500

    Day 9 pt 2

--- lib/day9.ex
+++ lib/day9.ex
@@ -10,19 +10,104 @@ defmodule Aoc.Day9 do
   def day(), do: 9
 
   @impl Day
-  def a(_) do
-    :ok
+  def a(grid) do
+    grid
+    |> coordinates()
+    |> Enum.filter(&low_point?(&1, grid))
+    |> Enum.map(&(value_at(&1, grid) + 1))
+    |> Enum.sum()
   end
 
   @impl Day
-  def b(_) do
-    :ok
+  def b(grid) do
+    grid
+    |> coordinates()
+    |> Enum.filter(&low_point?(&1, grid))
+    |> Enum.map(&basin_size(grid, [&1]))
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.product()
   end
 
   @impl Day
   def parse_input() do
     with {:ok, file} <- Day.load(__MODULE__) do
       file
+      |> String.split("\n", trim: true)
+      |> Enum.map(&binary_to_digits/1)
+      |> List.to_tuple()
+    end
+  end
+
+  def low_point?({_, _} = coord, grid) do
+    neighbours =
+      neighbours(coord)
+      |> Enum.map(&value_at(&1, grid))
+
+    value = value_at(coord, grid)
+
+    Enum.all?(neighbours, &(&1 > value))
+  end
+
+  def basin_size(grid, coords, size \\ 0, visited \\ MapSet.new())
+
+  def basin_size(_, [], size, _) do
+    size
+  end
+
+  def basin_size(grid, [coord | rest], size, visited) do
+    cond do
+      # Coordinate already visited, ignore
+      MapSet.member?(visited, coord) ->
+        basin_size(grid, rest, size, visited)
+
+      # Border of the basin
+      value_at(coord, grid) >= 9 ->
+        basin_size(grid, rest, size, MapSet.put(visited, coord))
+
+      # Add neighbours to the list of coordinates and increase size
+      true ->
+        basin_size(grid, rest ++ neighbours(coord), size + 1, MapSet.put(visited, coord))
+    end
+  end
+
+  def neighbours({x, y}) do
+    [{x, y - 1}, {x, y + 1}, {x - 1, y}, {x + 1, y}]
+  end
+
+  def outside_bounds({-1, _}, _, _), do: true
+  def outside_bounds({_, -1}, _, _), do: true
+  def outside_bounds({x, _}, max_x, _) when x > max_x, do: true
+  def outside_bounds({_, y}, _, max_y) when y > max_y, do: true
+  def outside_bounds(_, _, _), do: false
+
+  def binary_to_digits(string) do
+    string
+    |> String.codepoints()
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
+  end
+
+  # Instead of worrying about checking for overflowing the bounds of
+  # the grid, we can just return the highest possible value, in 
+  # this case 9.
+  def value_at({x, y}, grid)
+      when x < 0 or y < 0 or x >= tuple_size(elem(grid, 0)) or y >= tuple_size(grid) do
+    9
+  end
+
+  def value_at({x, y}, grid) do
+    grid
+    |> elem(y)
+    |> elem(x)
+  end
+
+  def coordinates(grid) do
+    x = tuple_size(elem(grid, 0)) - 1
+    y = tuple_size(grid) - 1
+
+    for y <- 0..y, x <- 0..x do
+      {x, y}
     end
   end
 end
